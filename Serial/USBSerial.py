4219a9bbbb767442fbcdd344d52f34e7ce03b3db
Chandra <1030819289@qq.com>
2022-09-29 21:33:43 +0800

    initialization

---
+++ Serial/USBSerial.py
@@ -0,0 +1,88 @@
+"""
+This file is the Serial Communications
+@Author: Chandrasekhar
+@Package: pyserial == 3.5
+"""
+
+import serial
+
+SerialDic = {}
+
+
+# Check Enable Port
+# import serial.tools.list_ports
+# port_list = list(serial.tools.list_ports.comports())
+# print(port_list)
+# if len(port_list) == 0:
+#    print('\033[1;31mENABLE PORT IS NONE\33[0m')
+# else:
+#     for i in range(0,len(port_list)):
+#         print(port_list[i])
+# portx = 0
+# baud =9600, outtime=0.4
+
+
+def usb2serial_init(portx, baud=9600, outtime=0.4, *args, **kwargs):
+    """
+    This is the initialization of the USB to Serial.
+
+    Parameters:
+        portx: the port, e.g. /dev/ttyUSB0" in Linux"; "COM5" in Windows
+        baud:
+            Default: 9600
+            Option: 9600,19200,38400,57600,115200
+        outtime: timeout
+            Default: 0.4
+
+    Return:
+        Return the state of the connection
+    """
+
+    global SerialDic
+    SerialDic = {'portx':portx, 'baud':baud, 'outtime':outtime}
+    if kwargs:
+        SerialDic.update(kwargs)
+    try:
+        ser = serial.Serial(portx, baud, timeout=outtime)
+        print("\033[1;33mThe port is Open\033[0m")
+        ser.close()
+        return True
+    except Exception as err:
+        print(f"\033[1;31m ====== Initialization Error ====== \n {err}\033[0m""")
+        return False
+
+
+def usb2serial_at(string):
+    """
+    This is the Test of the Serial, Command: AT
+
+    Parameters:
+        string: the word want to write
+    """
+
+    global SerialDic
+    try:
+
+        # portx = "COM5"
+        # #
+        # bps = 9600
+        # # Timeout: NONE(forever); UNIT: s(second)
+        # outtime = 0.4
+        # # 打开串口，并得到串口对象
+        ser = serial.Serial(SerialDic['portx'], SerialDic['baud'], timeout=SerialDic['outtime'])
+        ser.flushInput()  # Clean Cache
+        ser.write(f"{string}\r\n".encode())
+        SerialReturn_1 = ser.readline()
+        SerialReturn_2 = ser.readline()
+        # SerialReturn = str(SerialReturn, encoding='gbk')
+        print(str(SerialReturn_1) + "\n" + str(SerialReturn_2))
+        ser.close()
+
+    except Exception as err:
+        print(f"\033[1;31m ====== Error ====== \n {err}\033[0m""")
+
+
+
+if __name__ == "__main__":
+    usb2serial_init("COM5")
+    usb2serial_at("AT")
